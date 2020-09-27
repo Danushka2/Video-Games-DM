116cabcdb230ff41efad7a716617b3b502f77919
Danushka2 <dhanushkanuwan24@gmail.com>
2020-09-27 19:37:52 +0530

    dataset folder structure added

---
+++ dataset/cleanDs.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def clean_db():
+    x = "Cleaned database returned"
+    return x
---
+++ models/sampleModel.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Importing
+import dataset.cleanDs as dataset
+
+#assign cleaned database here and use it
+cleanedDS = dataset.clean_db()
+print(cleanedDS)
