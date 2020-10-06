024342c640e40706d118a6ac8a9625d2ea8cd357
Danushka2 <dhanushkanuwan24@gmail.com>
2020-10-07 00:53:38 +0530

    sales predict UI added

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #return render_template('index.html')
     return render_template('index.html', url ='/static/images/line_plot.jpg')
+
+@app.route('/predict-sales')
+def pSales():
+    return render_template('sale-prediction.html')
 
 
 
