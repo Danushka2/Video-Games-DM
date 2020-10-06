fb1a4642e5ecc0e19e16415f02c0d4ed4215c9e5
Danushka2 <dhanushkanuwan24@gmail.com>
2020-10-07 02:25:47 +0530

    connnected the test model with the UI

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pickle
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+model = pickle.load(open('testModel.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -13,6 +14,16 @@ def home():
 @app.route('/predict-sales')
 def pSales():
     return render_template('sale-prediction.html')
+
+@app.route('/p-sales',methods=['POST'])
+def predicSales():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('sale-prediction.html', prediction_text='$ {}'.format(output))
 
 
 
