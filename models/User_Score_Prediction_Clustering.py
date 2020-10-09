d6a27d42345788f8e54d064831cd3e8d521d5f5b
KasunHewagama <kghewagama1997@gmail.com>
2020-10-09 19:43:19 +0530

    removed non usable importing lines...!

--- models/User_Score_Prediction_Clustering.py
+++ models/User_Score_Prediction_Clustering.py
@@ -1,8 +1,6 @@
 # -- coding: utf-8 --
 
 import pickle
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from dataset.cleanDs import cleanDs
 from sklearn.preprocessing import LabelEncoder
